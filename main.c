16e596fc6b3fd2fef6d67dd733e6426da28e232e
daisvke <daisuketanigawa@live.fr>
2021-11-28 21:09:12 +0100

    before wsl hotel

--- main.c
+++ main.c
@@ -2,16 +2,17 @@
 
 typedef struct s_envv
 {
-	int id;
+	size_t id;
 	pthread_mutex_t *forks;
 	pthread_t *tid;
-	int n;
-	int p;
+	size_t n;
+	size_t p;
 }		t_envv;
 
 void	*f(t_envv *envv)
 {
-	int	id = envv->id;
+	size_t	id = envv->id;
+	printf("n in f: %ld, i = %ld\n", envv->n, envv->id);
 
 	if (id >0)
 		id = id - 1;
@@ -28,12 +29,12 @@ void	init(t_envv *envv)
 	envv->forks = malloc(envv->n * sizeof(pthread_mutex_t));
 	envv->tid = malloc(envv->n * sizeof(pthread_t));
 }
-
+/*
 
 int main()
 {
 	t_envv	*envv;
-	int i =0;
+	size_t i =0;
 
 	envv=malloc(sizeof(t_envv));
 
@@ -42,35 +43,11 @@ int main()
 	init(envv);
 		printf("before : p: %d\n",envv->p);
 
-		printf("before : n: %d\n",envv->n);
-	envv->n = 3;
-	for (i=0;i < envv->n;i++)
-	{
-		printf("i: %d, n: %d\n", i, envv->n);
-		envv->id = i;
-		pthread_create(&envv->tid[i], NULL, (void*)f, &envv);
-		printf("thread no %d created\n", i);
-	}
-	for (i=0;i<envv->n;i++)
-	{
-		pthread_join(envv->tid[i], NULL);
-		printf("thread no %d joined\n", i);
-	}
-}
-/*
-int main()
+printf("before : n: %d\n",envv->n);
+envv->n = 3;
+for (i=0;i < envv->n;i++)
 {
-	t_envv	*envv;
-	int i =0;
-	envv->n = 3;
-envv = malloc(sizeof(t_envv));;
-	init(envv);
-
-		printf("before : n: %d\n",envv->n);
-	envv->n = 3;
-	for (i=0;i < envv->n;i++)
-	{
-		printf("i: %d, n: %d\n", i, envv->n);
+printf("i: %d, n: %d\n", i, envv->n);
 		envv->id = i;
 		pthread_create(&envv->tid[i], NULL, (void*)f, &envv);
 		printf("thread no %d created\n", i);
@@ -82,3 +59,27 @@ envv = malloc(sizeof(t_envv));;
 	}
 }
 */
+
+int main()
+{
+	t_envv	envv;
+	int i =0;
+	envv.n = 3;
+	init(&envv);
+
+		printf("before : n: %d\n",envv.n);
+	envv.n = 3;
+	for (i=0;i < envv.n;i++)
+	{
+		printf("i: %d, n: %d\n", i, envv.n);
+		envv.id = i;
+		pthread_create(&envv.tid[i], NULL, (void*)f, &envv);
+		printf("thread no %d created\n", i);
+	}
+	for (i=0;i<envv.n;i++)
+	{
+		pthread_join(envv.tid[i], NULL);
+		printf("thread no %d joined\n", i);
+	}
+}
+
