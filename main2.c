e6497ec90767f07c8272c471c5f2a62626f7bb63
daisvke <daisuketanigawa@live.fr>
2021-11-29 07:33:58 +0100

    from bb

--- main2.c
+++ main2.c
@@ -5,23 +5,22 @@ typedef struct s_envv
 	size_t id;
 	pthread_mutex_t *forks;
 	pthread_t *tid;
+    pthread_mutex_t c;
 	size_t n;
 	size_t p;
-    pthread_mutex_t c;
 }		t_envv;
 
 void	*f(t_envv *envv)
 {
-    pthread_mutex_lock(&envv->c);
+pthread_mutex_lock(&envv->c);
 	size_t	id = envv->id;
-    pthread_mutex_unlock(&envv->c);
+pthread_mutex_unlock(&envv->c);
 	printf("n in f: %ld, i = %ld\n", envv->n, envv->id);
-    /*
 
 	if (id >0)
 		id = id - 1;
 	else
-		id = envv->n;*/
+		id = envv->n;
 	pthread_mutex_lock(&envv->forks[id]);
 	printf("this is the unlocked message from id: %d\n", id);
 	pthread_mutex_unlock(&envv->forks[id]);
@@ -29,8 +28,13 @@ void	*f(t_envv *envv)
 
 void	init(t_envv *envv)
 {
+
 		printf("in init : n: %d\n",envv->n);
 	envv->forks = malloc(envv->n * sizeof(pthread_mutex_t));
+    int i;
+    pthread_mutex_init(&envv->c,NULL);
+    for(i=0;i<envv->n;i++)
+        pthread_mutex_init(&envv->forks[i],NULL);
 	envv->tid = malloc(envv->n * sizeof(pthread_t));
 }
 /*
@@ -64,6 +68,14 @@ printf("i: %d, n: %d\n", i, envv->n);
 }
 */
 
+void geti(t_envv *envv,int *i)
+{
+pthread_mutex_lock(&envv->c);
+++i;
+		envv->id = *i;
+pthread_mutex_unlock(&envv->c);
+}
+
 int main()
 {
 	t_envv	envv;
@@ -73,10 +85,10 @@ int main()
 
 		printf("before : n: %d\n",envv.n);
 	envv.n = 3;
-	for (i=0;i < envv.n;i++)
+	for (i=-1;i < envv.n;i++)
 	{
 		printf("i: %d, n: %d\n", i, envv.n);
-		envv.id = i;
+        geti(&envv,&i);
 		pthread_create(&envv.tid[i], NULL, (void*)f, &envv);
 		printf("thread no %d created\n", i);
 	}
